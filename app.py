01c6e289084ef59c93280926045218dc326a2097
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-27 07:29:38 -0500

    carry over code from notebook

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import yfinance as yf
 
+
 df = yf.download("AAPL",
                  start="2011-01-01",
                  end="2021-12-31",
@@ -8,3 +9,7 @@ df = yf.download("AAPL",
 
 print(f"Downloaded {len(df)} rows of data.")
 print(df)
+
+aapl_data = yf.Ticker("AAPL")
+
+aapl_data.history()

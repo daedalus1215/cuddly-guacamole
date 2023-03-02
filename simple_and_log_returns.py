16703d745f8989d8ed4e6b3d5c3b106bd8ac9b13
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-01 21:09:47 -0500

    Add comments to the simple and log returns

--- simple_and_log_returns.py
+++ simple_and_log_returns.py
@@ -11,7 +11,14 @@ df = yf.download("AAPL",
 df = df.loc[:, ["Adj Close"]]
 
 # Calculate the simple and log returns using the adjusted close prices:
+
+# pct_change = calculates the percentage change between the current and prior element (we can specify the number of
+# lags, but for this specific case the default value of 1 suffices)
 df["simple_rtn"] = df["Adj Close"].pct_change()
+
+# To calc the log return, we use the log return formula. When dividing each elemebnt of the series by its lagged value,
+# we used the shift method with a calue of 1 to access the prior element. In the end we took the natural logarithm of
+# the divided values by using np.log func
 df["log_rtn"] = np.log(df["Adj Close"]/df["Adj Close"].shift(1))
 
 df.head()

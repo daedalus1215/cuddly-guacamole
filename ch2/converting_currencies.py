134c4c74f17b1cdba05ac4b567536550a4b42b12
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-04 10:23:19 -0500

    Add converting currency

---
+++ ch2/converting_currencies.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import yfinance as yf
+from forex_python.converter import CurrencyRates
+
+df = yf.download("AAPL",
+                 start="2020-01-01",
+                 end="2020-01-31",
+                 progress=False)
+df = df.drop(columns=["Adj Close", "Volume"])
+# Instantiate the Currency Rate Object
+c = CurrencyRates()
+
+[print(date) for date in df.index if date.weekday() < 5]
+# Download the USD/EUR rate for each required date:
+df["usd_eur"] = [c.get_rate("USD", "EUR", date) for date in df.index if date.weekday() < 5]
+# Convert the price from USD to EUR:
+for column in df.columns[:-1]:
+    df[f"{column}_EUR"] = df[column] * df["usd_eur"]
+
+print(df.head())

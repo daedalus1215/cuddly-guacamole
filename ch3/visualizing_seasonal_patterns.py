62f7dfb8f27232328aa52417d068127692cc1d2f
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-07 20:54:13 -0500

    polar seasonal plot using plotly.express

--- ch3/visualizing_seasonal_patterns.py
+++ ch3/visualizing_seasonal_patterns.py
@@ -4,6 +4,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from datetime import datetime
 import pytz
+from statsmodels.graphics.tsaplots import month_plot, quarter_plot
+import plotly.express as px
 
 nasdaqdatalink.read_key(filename="/data/nasdaqdatalinkkey")
 
@@ -31,12 +33,25 @@ plt.title("Unemployment rate - Seasonal plot")
 plt.legend(bbox_to_anchor=(1.05, 1), loc=2)
 # plt.show()
 
-
-from statsmodels.graphics.tsaplots import month_plot, quarter_plot
-import plotly.express as px
-
+# Create a month plot:
 month_plot(df["unemp_rate"], ylabel="Unemployment rate (%)")
 plt.title("Unemployment rate - Month plot")
+
+# Create a quarter plot:
 quarter_plot(df["unemp_rate"].resample("Q").mean(), ylabel="Unemployment rate (%)")
 plt.title("Unemployment rate - Quarter plot")
 plt.show()
+
+# Create a polar seasonal plot using plotly.express:
+fig = px.line_polar(
+    df,
+    r="unemp_rate",
+    theta="month",
+    color="year",
+    line_close=True,
+    title="Unemployment rate - Polar seasonal plot",
+    width=600,
+    height=500,
+    range_r=[3, 7],
+)
+fig.show()

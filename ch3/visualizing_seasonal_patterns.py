8692a800dc6805ab66b32ba4e9bb3e60aeb275e8
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-07 20:46:50 -0500

    seasonal unemployment, showing different years by month

--- ch3/visualizing_seasonal_patterns.py
+++ ch3/visualizing_seasonal_patterns.py
@@ -2,14 +2,37 @@ import pandas as pd
 import nasdaqdatalink
 import seaborn as sns
 import matplotlib.pyplot as plt
+from datetime import datetime
+import pytz
 
-nasdaqdatalink.read_key(filename='/data/nasdaqdatalinkkey')
+nasdaqdatalink.read_key(filename="/data/nasdaqdatalinkkey")
 
-df = (
-    nasdaqdatalink.get("FRED/UNRATENSA",
-                       start_date="2014-01-01",
-                       end_date="2019-12-31").rename(columns={"Value":"unemp_rate"})
+df = nasdaqdatalink.get(
+    "FRED/UNRATENSA", start_date="2014-01-01", end_date="2019-12-31"
+).rename(columns={"Value": "unemp_rate"})
+# fig, ax = plt.subplots(1, 1, sharex=True)
+df.plot(title="Unemployment rate in years 2014-2019")
+# plt.show()
+
+# create new columns with year and month
+df["year"] = df.index.year
+df["month"] = df.index.strftime("%b")
+
+sns.lineplot(
+    data=df,
+    x="month",
+    y="unemp_rate",
+    hue="year",
+    style="year",
+    legend="full",
+    palette="colorblind",
 )
-fig, ax = plt.subplots(2, 1, sharex=True)
-ax[0].plot(df)
+plt.title("Unemployment rate - Seasonal plot")
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2)
 plt.show()
+
+
+from statsmodels.graphics.tsaplots import month_plot, quarter_plot
+import plotly.express as px
+
+# month_plot(df["unemp_rate"], ylabel="Unemployment rate (%)") plt.title("Unemployment rate - Month plot")

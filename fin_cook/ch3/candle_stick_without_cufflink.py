ebb91921e732a4f284c0ba539906485ec9f1a5a7
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-11 07:53:36 -0500

    Create candle stick without cufflink

---
+++ fin_cook/ch3/candle_stick_without_cufflink.py
@@ -0,0 +1,20 @@
+import plotly.graph_objects as go
+import yfinance as yf
+
+df = yf.download(
+    "TWTR", start="2018-01-01", end="2018-12-31", progress=False, auto_adjust=True
+)
+
+fig = go.Figure(data=go.Candlestick(x=df.index,
+                                    open=df["Open"],
+                                    high=df["High"],
+                                    low=df["Low"],
+                                    close=df["Close"])
+                )
+
+fig.update_layout(
+    title="Twitter's stock prices in 2018",
+    yaxis_title="Price ($)"
+)
+
+fig.show()

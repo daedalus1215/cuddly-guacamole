66d982bef554ad459f62fe504c1287ad94b0da2e
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-30 16:46:17 -0400

    Add spike of 20 day MA

---
+++ fin_cook/ch5/ma.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+df = yf.download("IBM",
+                 start="2020-01-01",
+                 end="2020-12-31",
+                 progress=False,
+                 auto_adjust=True)
+
+# calculate and plot the SMA
+
+df["sma_20"] = df["Close"].rolling(window=20).mean()
+
+fig, ax = plt.subplots()
+df[["sma_20", "Close"]].plot(ax=ax)
+
+plt.show()
+
+# Calculate and plot the Bollinger Bands

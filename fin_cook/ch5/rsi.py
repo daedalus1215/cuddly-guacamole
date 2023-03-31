80a18a590e8a511de3e74bdaca68a8cff3a80131
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-31 14:45:45 -0400

    create rsi with ta-lib

--- fin_cook/ch5/rsi.py
+++ fin_cook/ch5/rsi.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import talib
 import yfinance as yf
 import matplotlib.pyplot as plt
 
@@ -8,33 +9,12 @@ df = yf.download("IBM",
                  progress=False,
                  auto_adjust=True)
 
-# Calculate and plot the Bollinger Bands
-close_delta = df['Close'].diff()
-
-# make two series: one for lower closes and one for higher closes
-up = close_delta.clip(lower=0)
-down = -1 * close_delta.clip(upper=0)
-
-periods = 14
-# exponential moving average
-# ma_up = up.ewm(com=periods-1, adjust=True, min_periods=periods).mean()
-# ma_down = down.ewm(com=periods-1, adjust=True, min_periods=periods).mean()
-
-# simple moving average
-ma_up = up.rolling(window=periods).mean()
-ma_down = down.rolling(window=periods).mean()
-
-rsi = ma_up / ma_down
-rsi = 100 - (100 / (1 + rsi))
+df["rsi"] = talib.RSI(df["Close"])
 
 fig, ax = plt.subplots()
-df['Close'].plot(ax=ax, title="Relative Strength Index (RSI)")
-ax.hlines(y=30,
-          xmin=df.index.min(),
-          xmax=df.index.max(),
-          color="red")
-ax.hlines(y=70,
-          xmin=df.index.min(),
-          xmax=df.index.max(),
-          color="red")
+df["rsi"].plot(ax=ax, title="Relative Strength Index (RSI)")
+
+ax.hlines(y=30, xmin=df.index.min(), xmax=df.index.max(), color="red")
+ax.hlines(y=70, xmin=df.index.min(), xmax=df.index.max(), color="red")
+
 plt.show()

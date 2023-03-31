7fe7b29dbb96afea18825b55a8773c09d851ba53
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-31 14:46:05 -0400

    create multiple indicators with ta library

---
+++ fin_cook/ch5/multiple_indicators.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import talib
+import yfinance as yf
+import matplotlib.pyplot as plt
+from ta import add_all_ta_features
+
+df = yf.download("IBM",
+                 start="2020-01-01",
+                 end="2020-12-31",
+                 progress=False,
+                 auto_adjust=True)
+
+# discard all previous calculated indicators
+df = df[["Open", "High", "Low", "Close", "Volume"]].copy()
+
+# calculate all the technical indicators available in the ta library
+df = add_all_ta_features(df,
+                         open="Open",
+                         high="High",
+                         low="Low",
+                         close="Close",
+                         volume="Volume")
+
+print(df)

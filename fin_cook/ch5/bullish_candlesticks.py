52d458dc5a92d339b84d34b90e107c740b8cb91d
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-04-03 23:35:29 -0400

    get streamlit to work

--- fin_cook/ch5/bullish_candlesticks.py
+++ fin_cook/ch5/bullish_candlesticks.py
@@ -17,4 +17,4 @@ for candle in candle_names:
     df["candle"] = getattr(talib, candle)(df["Open"], df["High"], df["Low"], df["Close"])
 
 with pd.option_context("display.max_rows", len(candle_names)):
-    display(df[candle_names].describe().transpose().round(2))
+    display(df[candle_names])
